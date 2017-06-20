f5b284ab1747732cc35095c7077518965949ba63
Filip Matzner <floop@floop.cz>
2017-06-20 23:31:52 +0200

    Update TensorFlow to v1.2.0

--- tensorflow_cc/cmake/TensorflowBase.cmake
+++ tensorflow_cc/cmake/TensorflowBase.cmake
@@ -3,7 +3,7 @@ include(ExternalProject)
 ExternalProject_Add(
   tensorflow_base
   GIT_REPOSITORY http://github.com/tensorflow/tensorflow.git
-  GIT_TAG v1.2.0-rc2
+  GIT_TAG v1.2.0
   TMP_DIR "/tmp"
   STAMP_DIR "tensorflow-stamp"
   DOWNLOAD_DIR "tensorflow"
