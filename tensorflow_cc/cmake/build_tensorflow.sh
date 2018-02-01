cdf9ce4cebef25fbf1ab90962d5144b8e6417f33
Filip Matzner <floop@floop.cz>
2018-02-01 20:10:19 +0100

    Improve the search for cudnn.h
    Closes #42

--- tensorflow_cc/cmake/build_tensorflow.sh
+++ tensorflow_cc/cmake/build_tensorflow.sh
@@ -23,11 +23,20 @@ export PYTHON_LIB_PATH="$($PYTHON_BIN_PATH -c 'import site; print(site.getsitepa
 if [ -e /opt/cuda ]; then
     echo "Using CUDA from /opt/cuda"
     export CUDA_TOOLKIT_PATH=/opt/cuda
-    export CUDNN_INSTALL_PATH=/opt/cuda
 elif [ -e /usr/local/cuda ]; then
     echo "Using CUDA from /usr/local/cuda"
     export CUDA_TOOLKIT_PATH=/usr/local/cuda
+fi
+
+if [ -e /opt/cuda/include/cudnn.h ]; then
+    echo "Using CUDNN from /opt/cuda"
+    export CUDNN_INSTALL_PATH=/opt/cuda
+elif [ -e /usr/local/cuda/cudnn.h ]; then
+    echo "Using CUDNN from /usr/local/cuda"
     export CUDNN_INSTALL_PATH=/usr/local/cuda
+elif [ -e /usr/include/cudnn.h ]; then
+    echo "Using CUDNN from /usr"
+    export CUDNN_INSTALL_PATH=/usr
 fi
 
 if [ -n "${CUDA_TOOLKIT_PATH}" ]; then
