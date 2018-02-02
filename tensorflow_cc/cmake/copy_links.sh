cafe59f66c5b3127a5973a29d297382ad9afbb8e
Filip Matzner <floop@floop.cz>
2018-02-02 10:41:27 +0100

    Fix missing space in copy_links

--- tensorflow_cc/cmake/copy_links.sh
+++ tensorflow_cc/cmake/copy_links.sh
@@ -6,7 +6,7 @@ set -e
 # To properly handle whitespace characters in filenames, we need to use
 # an ugly `find` and `read` trick.
 find "$1" -type l -print0 |
-    while IFS=read -r -d $'\0' f; do
+    while IFS= read -r -d $'\0' f; do
         # We need to check whether the file is still a link.
         # It may have happened that we have already replaced it by
         # the original when some of its parent directories were copied.
