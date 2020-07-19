40fb0155ba77aa86e09498bef1f54e67cf3e49d3
Mohammad-Reza <rmdaliri@gmail.com>
2020-07-19 06:24:38 -0600

    Add a bash script to automcatically install requirements on Ubuntu

---
+++ install-requirements.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+# install requirements
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y update
+apt-get -y install \
+  cmake \
+  g++-7 \
+  git \
+  python3-dev \
+  python3-numpy \
+  sudo \
+  wget
+
+# install bazel
+export BAZEL_VERSION=${BAZEL_VERSION:-`cat ./Dockerfiles/BAZEL_VERSION`}
+apt-get -y install pkg-config zip g++ zlib1g-dev unzip python
+bazel_installer=bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh
+wget -P /tmp https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/${bazel_installer}
+chmod +x /tmp/${bazel_installer}
+/tmp/${bazel_installer}
+rm /tmp/${bazel_installer}
+
+if [[ ${BUILD_WITH_CUDA:-"OFF"} == "ON" ]]; then
+    # install libcupti
+    apt-get -y install cuda-command-line-tools-10-1
+fi
