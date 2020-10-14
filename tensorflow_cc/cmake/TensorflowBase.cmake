8224cc463fc50e95a68ced954f639e557da014d2
Filip Matzner <floop@floop.cz>
2020-10-14 16:12:02 +0200

    Add bazel-bin/bin to the list of include dirs
    Closes #237

--- tensorflow_cc/cmake/TensorflowBase.cmake
+++ tensorflow_cc/cmake/TensorflowBase.cmake
@@ -13,4 +13,10 @@ ExternalProject_Add(
   CONFIGURE_COMMAND ""
   BUILD_COMMAND "${CMAKE_CURRENT_BINARY_DIR}/build_tensorflow.sh"
   INSTALL_COMMAND "${CMAKE_CURRENT_SOURCE_DIR}/cmake/copy_links.sh" bazel-bin
+          # The include folders are sometimes contained under bazel-bin/bin/ and sometimes just bazel-bin.
+          # Later, we include both so let's make sure they both exist so that CMake does not complain.
+          COMMAND mkdir -p "${CMAKE_CURRENT_BINARY_DIR}/tensorflow/bazel-bin/tensorflow/include/src"
+          COMMAND mkdir -p "${CMAKE_CURRENT_BINARY_DIR}/tensorflow/bazel-bin/bin/tensorflow/include/src"
+          COMMAND touch "${CMAKE_CURRENT_BINARY_DIR}/tensorflow/bazel-bin/tensorflow/include/src/__placeholder__.h"
+          COMMAND touch "${CMAKE_CURRENT_BINARY_DIR}/tensorflow/bazel-bin/bin/tensorflow/include/src/__placeholder__.h"
 )
